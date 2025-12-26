55626c3093f6af071cf7c571f48b65ff44d5cb87
saggabbb <bagasadigia@gmail.com>
2025-12-27 03:06:34 +0700

    seeder (belum divalidasi)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+          $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
+            OrderSeeder::class,
+            OrderDetailSeeder::class,
+            PaymentSeeder::class,
+          ]);
     }
 }
