8090d83871a30b7ae259e96c918a2fec19d54d24
saggabbb <bagasadigia@gmail.com>
2026-01-18 12:54:24 +0700

    fix: Properly close grid container to make ordered items full width

--- resources/views/seller/orders/show.blade.php
+++ resources/views/seller/orders/show.blade.php
@@ -131,7 +131,7 @@
                             </form>
                         @endif
                     </div>
-            
+                </div>
             </div>
 
             <!-- Products -->
