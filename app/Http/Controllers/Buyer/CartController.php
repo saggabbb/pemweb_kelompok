e04cd1a89d3ea85d7353df1d697d24f650ec1a9f
saggabbb <bagasadigia@gmail.com>
2026-01-18 20:56:33 +0700

    fix: Add OrderDetail import in CartController for checkout

--- app/Http/Controllers/Buyer/CartController.php
+++ app/Http/Controllers/Buyer/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Product;
 use App\Models\Order;
+use App\Models\OrderDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
